900b81053ade2233c1aa54bfa864373c42be10b0
wpohlhau <william.pohlhaus@villanova.edu>
2019-01-19 18:51:45 -0500

    Adding Stub class LunatecsDrive

---
+++ src/main/java/frc/robot/LunatecsDrive.java
@@ -0,0 +1,26 @@
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class LunatecsDrive {
+
+    private WPI_TalonSRX right;
+    private WPI_TalonSRX left;  
+    private DifferentialDrive drive;
+
+    public LunatecsDrive(WPI_TalonSRX left, WPI_TalonSRX right) {
+
+    }
+
+    public void arcadeDrive(double speed, double rotation, boolean rampUp) {
+
+    }
+
+    private void goStraight(double speed) {
+
+    }
+
+    private double rampUp(double speed) {
+        return 1.0;
+    }
+
+}
