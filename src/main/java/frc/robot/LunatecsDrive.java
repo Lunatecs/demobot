0480dfeee119e3e68e5c345ffa801e5a6082ec9b
TheDyingSun <thedyingsun.ng@gmail.com>
2019-01-19 19:49:31 -0500

    Added code for arcade drive and added some constants

--- src/main/java/frc/robot/LunatecsDrive.java
+++ src/main/java/frc/robot/LunatecsDrive.java
@@ -8,13 +8,44 @@ public class LunatecsDrive {
     private WPI_TalonSRX right;
     private WPI_TalonSRX left;  
     private DifferentialDrive drive;
+    private static final double DEADZONE = 0.2;
+    private boolean reset = true;
 
     public LunatecsDrive(WPI_TalonSRX left, WPI_TalonSRX right) {
-
+        //Here lies the souls of the robotics students
     }
 
     public void arcadeDrive(double speed, double rotation, boolean rampUp) {
+        if(Math.abs(speed) >= DEADZONE || Math.abs(rotation) >= DEADZONE){
+            double speed2 = speed;
+            if(rampUp && Math.abs(speed) > DEADZONE){
+                speed2 = rampUp(speed);
+            }
 
+            if(Math.abs(speed) >= DEADZONE && Math.abs(rotation) <= DEADZONE){
+                if(reset){
+                    calibrateEncodersToZero();
+                    reset = false;
+                }
+
+                goStraight(speed2);
+
+            } else {
+                drive.arcadeDrive(speed2, rotation);
+                reset = true;
+            }
+
+        } else {
+            drive.arcadeDrive(0,0);
+            reset = true;
+        }
+        
+    }
+
+    private void calibrateEncodersToZero(){
+            left.setSelectedSensorPosition(0,0,10);
+            right.setSelectedSensorPosition(0,0,10);
+        
     }
 
     private void goStraight(double speed) {
